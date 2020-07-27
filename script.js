b914c31ea54f81b7d4f1472d65a9940cba0d5253
inovik94 <>
2020-07-27 21:06:43 +0300

    upd7

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ $('.more-info').click(function(){
 $('#background').click(function(){
   $('#card').removeClass('flip');
 })
+
+document.onmousewheel = document.onwheel = function() {
+return false;
+};
+
+document.addEventListener ("MozMousePixelScroll",
+function() { return false }, false);
+
+document.onkeydown = function(e) {
+if (e.keyCode >= 33 && e.keyCode <= 40) return false;
+}
